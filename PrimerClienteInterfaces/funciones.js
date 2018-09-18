87041e42a6abf30c7b2ca14b56eb270bb76ee0fc
JvMolli <jaimemg@outlook.com>
2018-09-18 16:41:04 +0200

    first commit

---
+++ PrimerClienteInterfaces/funciones.js
@@ -0,0 +1,17 @@
+function isOdd(n){
+    if(n.length>5) return str;
+}
+
+function strFive(str){
+    if(str.length>5) return str;
+}
+
+function filter(a, f){
+  let ys = [];
+  for (let i of a){
+    if(f(a[i])){
+      ys.push(i);
+    }
+  }
+  return ys;
+}
